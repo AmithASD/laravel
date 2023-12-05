e0956b7a8e2d34081fdc18ec4868e2bb4937b89f
A_Sandaruwan <sandaruwan@portion.lk>
2023-12-05 22:06:57 +0530

    basic

--- database/migrations/2023_12_05_134933_create_customer_table.php
+++ database/migrations/2023_12_05_134933_create_customer_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('customer', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('address');
+            $table->string('designation');
+            $table->integer('age');
             $table->timestamps();
         });
     }
